3c6f3482dfca192eb5e8c371d3307a4c352af1cc
DevIGood <71323072+DevIGood@users.noreply.github.com>
2022-06-06 20:37:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,25 +7,34 @@ def draw(turtle, start, stop, step):
             turtle.goto(x * 20, y * 20)
             turtle.stamp()
 
+def redboxes(lock):
+    lock.acquire()
+    redB = Turtle(shape="square")
+    redB.shapesize(8)
+    redB.color("red")
+    redB.penup()
+    draw(redB, -12, 12, 12)
+    lock.release()
 
+def yellowboxes(lock):
+    lock.acquire()
+    yellowB = Turtle(shape="square")
+    yellowB.shapesize(4)
+    yellowB.color("gold")
+    yellowB.penup()
+    draw(yellowB, -6, 6, 12)
+    lock.release()
 
-redB = Turtle(shape="square")
-redB.shapesize(8)
-redB.color("red")
-redB.penup()
-draw(redB, -12, 12, 12)
+lock = threading.Lock()
 
+thread1 = threading.Thread(target=redboxes, args=(lock,))
+thread2 = threading.Thread(target=yellowboxes, args=(lock,))
 
+thread1.start()
+thread2.start()
 
-
-yellowB = Turtle(shape="square")
-yellowB.shapesize(4)
-yellowB.color("gold")
-yellowB.penup()
-draw(yellowB, -6, 6, 12)
-
-
-
+thread1.join()
+thread2.join()
 
 screen = Screen()
 screen.exitonclick()
